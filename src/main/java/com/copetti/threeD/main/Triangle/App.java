aa72111bcc0a4f70e52f231bf6252c28ff15cf0e
lhcopetti <lhcopetti@gmail.com>
2018-04-15 19:43:14 -0300

    Use the cyclic scene to start the App

--- src/main/java/com/copetti/threeD/main/Triangle/App.java
+++ src/main/java/com/copetti/threeD/main/Triangle/App.java
@@ -2,15 +2,10 @@ package com.copetti.threeD.main.Triangle;
 
 import com.copetti.threeD.game.GameWindow;
 import com.copetti.threeD.math.CPSize;
-import com.copetti.threeD.scenes.CubeScene;
+import com.copetti.threeD.scenes.CyclicSceneSequence;
 import com.copetti.threeD.window.DemoWindow;
 import com.copetti.threeD.window.GLFWWindowHandler;
 
-
-/**
- * Hello world!
- *
- */
 public class App
 {
 
@@ -21,8 +16,7 @@ public class App
 		w.setTitle("Window Title!");
 		w.centerWindow();
 
-		// GameWindow window = new GameWindow(w, new CyclicSceneSequence());
-		GameWindow window = new GameWindow(w, new CubeScene());
+		GameWindow window = new GameWindow(w, new CyclicSceneSequence());
 		window.start();
 	}
 
